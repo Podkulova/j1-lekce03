b552d7c40d630f3f480d9dc786dea25eebd00853
Podkulova <152322453+Podkulova@users.noreply.github.com>
2024-01-30 19:23:21 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -0,0 +1,18 @@
+package cz.czechitas.turtle;
+
+import dev.czechitas.java1.turtle.engine.Turtle;
+
+public class HlavniProgram {
+    private Turtle zofka;
+
+    public static void main(String[] args) {
+        new HlavniProgram().start();
+    }
+
+    public void start() {
+        zofka = new Turtle();
+
+        //TODO Tady bude kód pro kreslení želví grafiky.
+    }
+
+}
